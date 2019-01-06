f77233a760ee8e45f5433ed8b7b925442f5004e0
JarretYingling <44506154+JarretYingling@users.noreply.github.com>
2019-01-05 20:59:49 -0500

    incrementing counter and query offset for new gifs when you click the same button again

--- js/script.js
+++ js/script.js
@@ -3,16 +3,16 @@
 $(document).ready(function () {
 
     let Giphy = {
-        // &rating=G&lang=en
+        queryURL: "",
+        arrTopics: ["alcohol", "beer", "wine", "champagne", "whiskey", "bourbon", "rye", "scotch", "gin", "brandy", "cognac", "vodka", "moonshine"],
+        arrCounter: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        keep: false,
         arrQuery: ["https://api.giphy.com/v1/gifs/", "search?", "api_key=jspItT1f9xNw9QHrsUg5lrn5w5JVkIms", "&q=", "", "&limit=", 10, "&offset=", 0],
-        /* index map
+        /* query string legend
         4 = q search term
         6 = limit(10)
         8 = offset
         */
-        queryURL: "",
-        arrTopics: ["alcohol", "beer", "wine", "champagne", "whiskey", "bourbon", "rye", "scotch", "gin", "brandy", "cognac", "vodka", "moonshine"],
-        keep: false,
         displayButtons: function () {
             $("#buttons").empty();
             $.each(Giphy.arrTopics, function (index, value) {
@@ -22,13 +22,14 @@ $(document).ready(function () {
                 `);
             });
         },
-        makeURL: function (q) {
+        makeURL: function (q, counter) {
             this.arrQuery[4] = q;
+            this.arrQuery[8] = counter * this.arrQuery[6];
             this.queryURL = this.arrQuery.join("");
             return this.queryURL;
         },
-        queryGiphy: function (id) {
-            this.makeURL(id);
+        queryGiphy: function (id, counter) {
+            this.makeURL(id, counter);
             $.ajax({
                 url: this.queryURL,
                 method: "GET"
@@ -55,8 +56,12 @@ $(document).ready(function () {
 
     $(document).on("click", ".btn", function (event) {
         let id = $(this).attr("id");
-        console.log(id);
-        Giphy.queryGiphy(id);
+        let index = Giphy.arrTopics.indexOf(id);
+        let counter = Giphy.arrCounter[index];
+        console.log(id, index, counter);
+        Giphy.queryGiphy(id, counter);
+        Giphy.arrCounter[index]++;
+        console.log(Giphy.arrCounter);
     });
 
     $("#submit").on("click", function (event) {
@@ -64,29 +69,30 @@ $(document).ready(function () {
         let newTopic = $("#topic").val();
         console.log(newTopic);
         Giphy.arrTopics.push(newTopic);
+        Giphy.arrCounter.push(0);
         console.log(Giphy.arrTopics);
         Giphy.displayButtons();
         $("#topic").val("");
     });
 
     $(document).on("click", ".gif", function (event) {
-        
+
         if ($(this).attr("id") == "0") {
             let still = $(this).attr("src");
             let gif = $(this).attr("data-alt");
-            console.log(gif);
+            console.log(still, gif);
             $(this).attr("data-alt", still);
             $(this).attr("src", gif);
             $(this).attr("id", "1");
         } else if ($(this).attr("id") == "1") {
             let still = $(this).attr("data-alt");
             let gif = $(this).attr("src");
-            console.log(still);
+            console.log(still, gif);
             $(this).attr("data-alt", gif);
             $(this).attr("src", still);
             $(this).attr("id", "0");
         };
-        
+
     });
 
 });
